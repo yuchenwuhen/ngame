cf14031f1a149cf7b9114f14eb5b35805962d710
TechartistWang <zzwang1993@gmail.com>
2018-09-21 02:28:54 +0800

    结局

---
+++ Assets/Scripts/GameOverPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverPanel : MonoBehaviour {
+
+    public List<int> collectionList;
+    public GameObject iconAnimPrefab;
+    public Sprite[] sprites;
+    float timer = 0;
+    // Use this for initialization
+    void Start () {
+        collectionList.Add(0);
+        collectionList.Add(3);
+        collectionList.Add(5);
+        //InitAnimation(collectionList);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(collectionList.Count > 0)
+        {
+            timer += Time.deltaTime;
+            Debug.Log(collectionList.Count);
+            if (timer >= 2)
+            {
+                GameObject iconAnim = Instantiate(iconAnimPrefab, this.transform.parent);
+                iconAnim.transform.Find("jumpAnim").GetComponent<Image>().sprite = sprites[collectionList[0]];
+                if(collectionList.Count > 0)
+                {
+                    collectionList.RemoveAt(0);
+                }
+                
+                timer = 0;
+            }
+        }
+	}
+
+}
